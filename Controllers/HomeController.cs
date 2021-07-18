5cf7d82b2475384903441ebfdb7e460fc01a3bce
XshubhamX <85957124+shubham-bhardwaj07@users.noreply.github.com>
2021-07-18 14:21:40 +0530

    beakout

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Management.Controllers
+{
+    public class HomeController
+    {
+
+        public string Index()
+        {
+            return "hey from mvc";
+        }
+    }
+}
